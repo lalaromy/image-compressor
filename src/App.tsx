3ab84d00c04f202a7b4dc2dc2fc2ff8a4637f249
romyabbrederis <romy.abbrederis@gmail.com>
2021-01-20 20:21:22 +0100

    cleaned up

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import React, { Fragment } from 'react';
 import Upload from './components/Upload';
-import "tailwindcss/tailwind.css"
 import Header from './components/Header';
 
 export default function App(props: any): JSX.Element {
